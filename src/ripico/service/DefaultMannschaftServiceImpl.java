ed709fe97fa000a0a52a058d772dbc3071c4485f
Phil <zesarex@gmail.com>
2019-05-22 22:59:42 +0200

    - Added DefaultMannschaftServiceImpl

--- src/ripico/service/DefaultMannschaftServiceImpl.java
+++ src/ripico/service/DefaultMannschaftServiceImpl.java
@@ -1,13 +1,18 @@
 package ripico.service;
 
+import ripico.api.dal.MannschaftAdapter;
+import ripico.api.dal.MitarbeiterAdapter;
 import ripico.api.domain.Mannschaft;
 import ripico.api.service.MannschaftService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DefaultMannschaftServiceImpl implements MannschaftService {
+    private MannschaftAdapter mannschaftAdapter;
+
     @Override
     public List<Mannschaft> alleMannschaften() {
-        return null; // TODO SIMON MACH FIX
+        return mannschaftAdapter.alleMannschaften();
     }
 }
